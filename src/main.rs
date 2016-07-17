b03952bdedf3bb9eeda05595abe085014c8bbf93
Adam <sector-f@users.noreply.github.com>
2016-07-17 17:55:22 -0400

    Added query handling

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,15 @@ fn is_command(trigger: char, message: &str) -> bool {
     }
 }
 
-// fn get_reply_target(prefix: Option<String>, target: &str, nickname: &str) -> String {
-//     if target == nickname {
-//         let prefix = prefix.unwrap_or("".to_owned());
-//         let target_nick = prefix.split('!').nth(0).unwrap_or("");
-//         String::from(target_nick)
-//     } else {
-//         String::from(target)
-//     }
-// }
+fn get_reply_target(prefix: Option<String>, target: &str, nickname: &str) -> String {
+    if target == nickname {
+        let prefix = prefix.unwrap_or("".to_owned());
+        let target_nick = prefix.split('!').nth(0).unwrap_or("");
+        String::from(target_nick)
+    } else {
+        String::from(target)
+    }
+}
 
 fn get_commands(list_path: &Path) -> Result<BTreeMap<String, PathBuf>, io::Error> {
     if ! list_path.is_file() {
@@ -61,8 +61,17 @@ fn get_commands(list_path: &Path) -> Result<BTreeMap<String, PathBuf>, io::Error
     Ok(commands)
 }
 
+fn run_command(command: &Path) -> Option<String> {
+    match SysCommand::new("sh").arg("-c").arg(command).output() {
+        Ok(output) => {
+            String::from_utf8(output.stdout).ok()
+        }
+        Err(_) => None,
+    }
+}
+
 fn main() {
-    let commands = get_commands(Path::new("commands/list.txt")).unwrap();
+    let commands = get_commands(Path::new("commands/list.txt")).expect("Failed to get commands list");
 
     let nickname = "rustmachine".to_string();
     let server = "irc.rizon.net".to_string();
@@ -91,11 +100,26 @@ fn main() {
                 Command::PRIVMSG(target, message) => {
                     let message = message.trim_right();
                     if target == server.current_nickname() {
+                        let target = get_reply_target(reply.prefix, &target, &server.current_nickname());
+                        if message.starts_with(trigger) {
+                            if let Some(func) = commands.get(&message.split_whitespace().nth(0).unwrap_or("")[1..]) {
+                                if let Some(output) = run_command(&func) {
+                                    let _ = server.send_privmsg(&target, &output);
+                                }
+                            }
+                        } else {
+                            if let Some(func) = commands.get(&message.split_whitespace().nth(0).unwrap_or("")[0..]) {
+                                if let Some(output) = run_command(&func) {
+                                    let _ = server.send_privmsg(&target, &output);
+                                }
+                            }
+                        }
                     } else {
                         if is_command(trigger, message) {
                             if let Some(func) = commands.get(&message.split_whitespace().nth(0).unwrap_or("")[1..]) {
-                                let output = String::from_utf8(SysCommand::new(func).output().unwrap().stdout).unwrap();
-                                let _ = server.send_privmsg(&target, &output);
+                                if let Some(output) = run_command(&func) {
+                                    let _ = server.send_privmsg(&target, &output);
+                                }
                             }
                         }
                     }
